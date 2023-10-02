ef47e70b85aa689cf4ec3719ece1317a71d28b51
Samuel-seneni <samuelmunialo2005@gmail.com>
2023-10-02 13:33:17 -0700

    raed the text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - read text file print to stdout.
+ * @letters: number of letters.
+ * @filename: file being read.
+ * Return: actual number of bytes.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t r, w;
+
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	r = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
+
+	free(buf);
+	close(fd);
+	return (w);
+}
+
