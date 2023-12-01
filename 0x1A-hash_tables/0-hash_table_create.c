508cb6f07341fad8208661c100ab3cccbaeee4eb
Samuel seneni <samuelmunialo2005@gmail.com>
2023-12-01 02:16:35 -0800

    creating of a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,11 +11,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 
 	if (size == 0)
-		return (NULL);	
+		return (NULL);
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
-
 	table->size = size;
 	table->array = malloc(sizeof(hash_table_t));
 	if (table->array == NULL)
